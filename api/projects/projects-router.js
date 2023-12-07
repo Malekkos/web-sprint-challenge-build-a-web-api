3e61bbcec330d3f29ae81ca375eadcfd3534d2c4
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-06 21:27:23 -0800

    The above, forgot to save

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,27 @@
 // Write your "projects" router here!
 const express = require("express")
-
 // const { } = require("./projects-middleware") // middleware import once done
-
 const Projects = require("./projects-model")
+const router = express.Router()
+
+
+router.get("/", (req, res, next) => {
+
+})
+
+router.get("/:id", (req, res, next) => {
+
+})
+
+router.post("/", (req, res, next) => {
+
+})
+router.put("/:id", (req, res, next) => {
+
+})
+router.delete("/:id", (req, res, next) => {
+
+})
+router.get("/:id/actions", (req, res, next) => {
+  
+})
