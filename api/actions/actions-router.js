f30278dc6a22f3b6ff874f242116d818ac7f01c3
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-06 21:36:03 -0800

    Added the preliminary actions endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,34 @@
 // Write your "actions" router here!
+const express = require("express")
+//const { } = require("./actions-middleware") for once actions middleware is fleshed out
+const Actions = require("./actions-model")
+const router = express.Router()
+
+router.get("/", (req, res, next) => {
+
+})
+
+router.get("/:id", (req, res, next) => {
+
+})
+
+router.post("/", (req, res, next) => {
+
+})
+
+router.put("/:id", (req, res, next) => {
+
+})
+
+router.delete("/:id", (req, res, next) => {
+
+})
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+    customeMessage: "Something bad has happened inside of the actions router"
+  })
+})
+
+module.exports = router
