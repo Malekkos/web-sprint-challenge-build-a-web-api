714ce3eb53d9ee6e954f94a46beb07b8e8081dbf
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-07 20:54:20 -0800

    Created a 'validateRequestBody' middleware and a endpoint for post. Tests passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -15,7 +15,21 @@ async function validateUserId(req, res, next) {
   next()
 }
 
+async function validateRequestBody(req, res, next) {
+  console.log("Validating request body...")
+  const body = req.body
+  console.log("this is the body", body)
+  if(!body.name || !body.description) {
+    console.log("there is incompleted fields")
+  } else {
+    req.name = req.body.name
+    req.description = req.body.description
+    req.completed = req.body.completed
+  }
+  next()
+}
 
 module.exports = {
-  validateUserId
+  validateUserId,
+  validateRequestBody
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,6 @@
 // Write your "projects" router here!
 const express = require("express")
-const { validateUserId } = require("./projects-middleware") // middleware import once done
+const { validateUserId, validateRequestBody } = require("./projects-middleware") // middleware import once done
 const Projects = require("./projects-model")
 const router = express.Router()
 
@@ -32,8 +32,21 @@ router.get("/:id", validateUserId, (req, res, next) => {
 })
 
 
-router.post("/", (req, res, next) => {
-
+router.post("/", validateRequestBody, (req, res, next) => {
+  console.log("this is the req.name", req.name, "this is the req.description", req.description)
+  if(!req.name || !req.description) {
+    res.status(400).json({
+      message: "You fooled yourself!"
+    })
+  } else {
+  Projects.insert({name: req.name, description: req.description, completed: req.completed})
+  .then(project => {
+    res.status(201).json(project)
+  })
+  .catch(error => {
+    next(error)
+  })
+}
 })
 
 router.put("/:id", (req, res, next) => {
